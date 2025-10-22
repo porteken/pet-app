80cd1ff72d2d05f0b6f3da746d745f1b2c36dd23
Kenneth Porter <porteken@gmail.com>
2025-10-21 22:16:43 -0400

    Update playwright config command to use "npm run dev" instead of "npx run dev".

--- playwright.config.ts
+++ playwright.config.ts
@@ -22,7 +22,7 @@ export default defineConfig({
   },
 
   webServer: {
-    command: "npx run dev",
+    command: "npm run dev",
     reuseExistingServer: !process.env.CI,
     timeout: 180 * 1000,
     url: "http://localhost:3000",

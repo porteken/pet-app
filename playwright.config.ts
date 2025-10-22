039b2e2c670f972effe8cba1524541f98878c2aa
Kenneth Porter <porteken@gmail.com>
2025-10-21 22:13:00 -0400

    Update build step in pre-push script and adjust web server command in playwright config file.

--- playwright.config.ts
+++ playwright.config.ts
@@ -22,7 +22,7 @@ export default defineConfig({
   },
 
   webServer: {
-    command: "npx next start",
+    command: "npm start",
     reuseExistingServer: !process.env.CI,
     timeout: 180 * 1000,
     url: "http://localhost:3000",

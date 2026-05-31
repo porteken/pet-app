4bc85cf5425ae9b0a726553a1a582cf903d909e7
Kenneth Porter <porteken@gmail.com>
2026-05-31 16:28:11 -0400

    add Vitest tests for database queries and configuration setup

---
+++ vitest.db.config.ts
@@ -0,0 +1,24 @@
+/// <reference types="vitest" />
+import { fileURLToPath, URL } from "node:url";
+
+import { defineConfig } from "vitest/config";
+
+export default defineConfig({
+  resolve: {
+    alias: {
+      "@": fileURLToPath(new URL("src", import.meta.url)),
+    },
+  },
+  test: {
+    environment: "jsdom",
+    globalSetup: ["./src/testing/global-setup.ts"],
+    globals: true,
+    include: [
+      "src/**/*.db.test.js",
+      "src/**/*.db.test.jsx",
+      "src/**/*.db.test.ts",
+      "src/**/*.db.test.tsx",
+    ],
+    setupFiles: ["./vitest.setup.ts"],
+  },
+});
